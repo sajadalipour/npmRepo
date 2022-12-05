fc3f7f4cf11b499b4b19ee3f74ffb077a2b0db22
saji <sajad.alipour@gmail.com>
2022-12-05 21:30:49 +0900

    fifth  commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn new thingss
+          Learn new thingsss
         </a>
       </header>
     </div>
