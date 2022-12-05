bf5a5cfc5488ddbcd451cfc14a171e9fb81e7883
sajadalipour <sajad.alipour@gmail.com>
2022-12-05 21:38:58 +0900

    joon commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn new thingsss Saji jooon
+          Learn new thingsss Saji jooon jooon
         </a>
       </header>
     </div>
